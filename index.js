d5a0f55bf7c8cf55e0caca222ab5de0548037037
Josh Fuller <joshf5252@gmail.com>
2021-12-12 21:41:35 -0500

    commit tree ccaf86c3126e360d3dcb5bba24caa3a3d610e04f

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
+  if (someValue>2500) {
+    return 'No can do.';
+  } if (someValue>2000) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (someValue<=400) {
+    return 'This one is on me!';
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+return city === `NYC` ? `Ok, sounds good.`:`No go.`;
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case "generous":
+    return "Thank you so much.";
+    case "not as generous": 
+    return "Thank you.";
+    default: 
+    return "Bye.";
+
+  }
   // Write your code here!
 }
